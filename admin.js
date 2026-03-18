a990c1bf33312333dab319119f33667a0d2609c2
darknesabc <161279889+darknesabc@users.noreply.github.com>
2026-03-18 20:22:58 +0900

    Update admin.js

--- admin.js
+++ admin.js
@@ -1651,6 +1651,7 @@ function renderTrendChart_(items) {
   }
   }
   });
+  };
 
   // [전체 (누적)] 토글 버튼
   const allBtn = document.createElement("button");
@@ -2009,4 +2010,4 @@ const lampHtml = `<div style="width:10px; height:10px; border-radius:50%; backgr
     panel.style.display = "block";
   };
 
-}; // 💡 핵심: 반드시 }); 로 끝나야 합니다!
+}); // 💡 핵심: 반드시 }); 로 끝나야 합니다!
